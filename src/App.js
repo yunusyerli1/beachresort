47788b97a66ecec8b0b62406c42d288a79ca2c17
unknown <yunusyerli1@gmail.com>
2020-06-18 16:40:54 +0300

    first-structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-
-//import './App.css';
+import {Route, Switch} from 'react-router-dom';
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+import Navbar from './components/Navbar'
+import './App.css';
 
 function App() {
   return (
    <>
-   Hello App
+    <Navbar/>
+    <Switch> 
+      <Route exact path="/" component= {Home} /> 
+      <Route exact path="/rooms" component= {Rooms} /> 
+      <Route exact path="/rooms/:id" component= {SingleRoom} /> 
+      <Route component={Error} />
+    </Switch>
    </>
   );
 }
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class SingleRoom extends Component {
+    render() {
+        return (
+            <div>
+                Hello from SINGLE ROOM Page 
+            </div>
+        )
+    }
+}
+export default SingleRoom;
